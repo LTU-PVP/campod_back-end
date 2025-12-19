eda6bf4dfbab6b80a87567db20cc2a1ef8dc1f96
Albin <albin@bohl.in>
2025-12-19 11:17:05 +0100

    one static url to play pink Floyd Time

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from justhtml import JustHTML
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,7 +18,13 @@ def player_page():
         print(p.name)        # "p"
         print(p.attrs)       # {"class": "intro"}
         print(p.to_html())   # <p class="intro">Hello!</p>
-    return doc.query("p.intro")[0].to_html()
+    
+    return doc.to_html()
+
+#http://127.0.0.1:5000/audio/Pink_Floyd_Time.mp3
+@app.route('/audio/<path:Pink_Floyd_Time>')
+def serve_floyd(Pink_Floyd_Time):
+        return send_from_directory('static/audio', Pink_Floyd_Time)
 
 if __name__ == '__main__':
     app.run()
